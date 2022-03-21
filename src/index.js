07d4d09fc9603cf2ad02fc0dcd77f2cb9bf89d25
Nesko <nseko@techaffinity.com>
2022-03-21 08:25:08 +0200

    locarazation refactoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<div>loading.....</div>} >
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
